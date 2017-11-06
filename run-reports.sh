1ae03ca35c286dba901d930c0a985e142810800f
Ho-Ling Poon <holingpoon@nypl.org>
2017-11-06 16:18:38 -0500

    SBAR-34 Changes include
    * Adding docker instructions for deployment on README.
    * Adding a sample environment variable file
    * Updating run-reports script to stop print output and uses correct environment variable names that matches with initial README.

--- run-reports.sh
+++ run-reports.sh
@@ -1,2 +1,1 @@
-ruby $OUTPUT_DIRECTORY/script/create_analytics_report.rb -i $GA_ID -a $AUTH  -s $START_DATE -e $END_DATE -o $OUTPUT_DIRECTORY
-cat $OUTPUT_DIRECTORY/output_2017-11-01_2017-11-02.csv
+ruby $OUTPUT/script/create_analytics_report.rb -i $ID -a $AUTH  -s $STARTDATE -e $ENDDATE -o $OUTPUT

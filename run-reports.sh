7db4a5662892df2d938906dc183eceb43fb9efbf
Ho-Ling Poon <holingpoon@nypl.org>
2017-11-06 11:35:24 -0500

    SBAR-34 Adding run-report.

--- run-reports.sh
+++ run-reports.sh
@@ -1,2 +1,2 @@
-ruby /app/script/create_analytics_report.rb -i ga:84655146 -a /app/config/google_auth.json -s 2017-11-01 -e 2017-11-02 -o /app
-cat /app/output_2017-11-01_2017-11-02.csv
+ruby $OUTPUT_DIRECTORY/script/create_analytics_report.rb -i $GA_ID -a $AUTH  -s $START_DATE -e $END_DATE -o $OUTPUT_DIRECTORY
+cat $OUTPUT_DIRECTORY/output_2017-11-01_2017-11-02.csv

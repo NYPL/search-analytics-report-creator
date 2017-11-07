d84c503901b1c0a9e8b70ede65e8ee43bbda8c06
Ho-Ling Poon <holingpoon@nypl.org>
2017-11-07 11:51:37 -0500

    Rolling back changes on run-reports.sh

--- run-reports.sh
+++ run-reports.sh
@@ -1,2 +1,2 @@
-ruby $OUTPUT/script/create_analytics_report.rb -i $ID -a $AUTH  -s $STARTDATE -e $ENDDATE -o $OUTPUT
-cat $OUTPUT/output_${STARTDATE}_${ENDDATE}.csv
+ruby /app/script/create_analytics_report.rb -i ga:84655146 -a /app/config/google_auth.json -s 2017-11-01 -e 2017-11-02 -o /app
+cat /app/output_2017-11-01_2017-11-02.csv

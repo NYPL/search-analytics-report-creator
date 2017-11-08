d03c759b2071a8d82030a608d5e05761819e0b3f
Ho-Ling Poon <holingpoon@nypl.org>
2017-11-08 14:51:11 -0500

    SBAR-39 Adding command line arguments to write to Google Sheets in run-reports.sh.

--- run-reports.sh
+++ run-reports.sh
@@ -1,1 +1,1 @@
-ruby /app/script/create_analytics_report.rb -i $ID -a $AUTH  -s $STARTDATE -e $ENDDATE -o $OUTPUT
+ruby /app/script/create_analytics_report.rb -i $ID -a $AUTH  -s $STARTDATE -e $ENDDATE -o $OUTPUT -g $PARENTFOLDERID

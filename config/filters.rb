e0b6a991657822d3cf3c555928154ee4535f7b66
Stephen Betts <thisisstephenbetts@gmail.com>
2017-12-06 11:12:29 -0500

    Just added a candidate filters config file

---
+++ config/filters.rb
@@ -0,0 +1,22 @@
+{
+  filters: [
+    {
+      title: 'All Query Summary',
+      searched_repo: ['ALL'],
+    },
+    {
+      title: 'Encore — Header Search vs Browse',
+      searched_repo: ['Encore'], 
+      searched_from: ['HeaderSearch', 'EncoreSearchForm'],
+    },      
+    {
+      title: 'Header Search',
+      searched_repo: ['Encore', 'BetaSearch', 'DrupalSearch'],
+      searched_from: ['HeaderSearch'],
+    },
+    {
+      title: 'Unknown where searched from',
+      searched_from: ['Unknown', 'NotSet']
+    },
+  ]
+}
